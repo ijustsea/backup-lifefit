7d29d00e846752110c56e150846f33c2dd939417
ijustsea <wlsqkd1199@naver.com>
2025-12-02 14:30:47 +0900

    fix: retest

--- lifeFit/src/main/java/com/kh/lifeFit/LifeFitApplication.java
+++ lifeFit/src/main/java/com/kh/lifeFit/LifeFitApplication.java
@@ -10,7 +10,6 @@ public class LifeFitApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(LifeFitApplication.class, args);
-        System.out.println("text");
 	}
 
 }
