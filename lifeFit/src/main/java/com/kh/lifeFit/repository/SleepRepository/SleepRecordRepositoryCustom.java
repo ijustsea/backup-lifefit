36a0fa743c16c7588eaa8764fc51d78eda837594
daeun567 <ekdmsdl336@gmail.com>
2025-12-12 16:15:25 +0900

    🔧 chore: 수면 관리 기능 삭제

--- lifeFit/src/main/java/com/kh/lifeFit/repository/SleepRepository/SleepRecordRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.kh.lifeFit.repository.SleepRepository;
-
-import com.kh.lifeFit.domain.sleepManager.SleepRecord;
-
-public interface SleepRecordRepositoryCustom {
-    SleepRecord saveSleepRecord(SleepRecord record);
-}
