ce590d93967a5e824b725bea3449d8a59033d795
Hyunah93 <fkqpsejzltm@naver.com>
2025-12-18 16:48:19 +0900

    🔥 remove: 심박수 알림 내역 Repository 삭제

--- lifeFit/src/main/java/com/kh/lifeFit/repository/heartDataRepository/HeartRateAlertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kh.lifeFit.repository.heartDataRepository;
-
-import com.kh.lifeFit.domain.heartData.HeartRateAlert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeartRateAlertRepository extends JpaRepository<HeartRateAlert, Long> {
-}
